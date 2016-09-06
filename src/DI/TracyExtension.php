186e39cd00b231cc418dae14b05f454cc147c64f
attreid <attreid@gmail.com>
2016-09-06 10:19:05 +0200

    odsazeni

--- src/DI/TracyExtension.php
+++ src/DI/TracyExtension.php
@@ -6,38 +6,41 @@ use NAttreid\TracyPlugin\Tracy;
 
 /**
  * Nastaveni Tracy
- * 
+ *
  * @author Attreid <attreid@gmail.com>
  */
-class TracyExtension extends \Nette\DI\CompilerExtension {
+class TracyExtension extends \Nette\DI\CompilerExtension
+{
 
-    private $defaults = [
-        'cookie' => NULL,
-        'mailPath' => '%tempDir%/mail-panel-mails',
-        'mailPanel' => TRUE
-    ];
+	private $defaults = [
+		'cookie' => NULL,
+		'mailPath' => '%tempDir%/mail-panel-mails',
+		'mailPanel' => TRUE
+	];
 
-    public function loadConfiguration() {
-        $builder = $this->getContainerBuilder();
-        $config = $this->validateConfig($this->defaults, $this->getConfig());
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+		$config = $this->validateConfig($this->defaults, $this->getConfig());
 
-        if (!isset($config['cookie'])) {
-            throw new \Nette\InvalidStateException("TracyPlugin: 'cookie' does not set in config.neon");
-        }
+		if (!isset($config['cookie'])) {
+			throw new \Nette\InvalidStateException("TracyPlugin: 'cookie' does not set in config.neon");
+		}
 
-        $config['mailPath'] = \Nette\DI\Helpers::expand($config['mailPath'], $builder->parameters);
+		$config['mailPath'] = \Nette\DI\Helpers::expand($config['mailPath'], $builder->parameters);
 
-        $builder->addDefinition($this->prefix('tracyPlugin'))
-                ->setClass(Tracy::class)
-                ->setArguments([$config['cookie']])
-                ->addSetup('setMail', [ $config['mailPath'], $config['mailPanel']]);
-    }
+		$builder->addDefinition($this->prefix('tracyPlugin'))
+			->setClass(Tracy::class)
+			->setArguments([$config['cookie']])
+			->addSetup('setMail', [$config['mailPath'], $config['mailPanel']]);
+	}
 
-    public function afterCompile(\Nette\PhpGenerator\ClassType $class) {
-        $initialize = $class->methods['initialize'];
-        if (class_exists('Tracy\Debugger')) {
-            $initialize->addBody('$this->getService(?)->run();', [$this->prefix('tracyPlugin')]);
-        }
-    }
+	public function afterCompile(\Nette\PhpGenerator\ClassType $class)
+	{
+		$initialize = $class->methods['initialize'];
+		if (class_exists('Tracy\Debugger')) {
+			$initialize->addBody('$this->getService(?)->run();', [$this->prefix('tracyPlugin')]);
+		}
+	}
 
 }
--- src/Tracy.php
+++ src/Tracy.php
@@ -2,115 +2,124 @@
 
 namespace NAttreid\TracyPlugin;
 
-use Nette\DI\Container,
-    Nextras\MailPanel\MailPanel,
-    Nextras\MailPanel\FileMailer,
-    Nette\Http\Request,
-    Nette\Http\Response,
-    Nette\Configurator,
-    Tracy\Debugger;
+use Nette\Configurator;
+use Nette\DI\Container;
+use Nette\Http\Request;
+use Nette\Http\Response;
+use Nextras\MailPanel\FileMailer;
+use Nextras\MailPanel\MailPanel;
+use Tracy\Debugger;
 
 /**
  * Nastaveni pro Tracy
  *
  * @author Attreid <attreid@gmail.com>
  */
-class Tracy {
+class Tracy
+{
 
-    use \Nette\SmartObject;
+	use \Nette\SmartObject;
 
-    /** @var string */
-    private $cookie;
+	/** @var string */
+	private $cookie;
 
-    /** @var string */
-    private $mailPath;
+	/** @var string */
+	private $mailPath;
 
-    /** @var boolean */
-    private $mailPanel;
+	/** @var boolean */
+	private $mailPanel;
 
-    /** @var Container */
-    private $container;
+	/** @var Container */
+	private $container;
 
-    /** @var Request */
-    private $request;
+	/** @var Request */
+	private $request;
 
-    /** @var Response */
-    private $response;
+	/** @var Response */
+	private $response;
 
-    /** @var boolean */
-    private $enable;
+	/** @var boolean */
+	private $enable;
 
-    public function __construct($cookie, Container $container, Request $request, Response $response) {
-        $this->cookie = $cookie;
-        $this->container = $container;
-        $this->request = $request;
-        $this->response = $response;
+	public function __construct($cookie, Container $container, Request $request, Response $response)
+	{
+		$this->cookie = $cookie;
+		$this->container = $container;
+		$this->request = $request;
+		$this->response = $response;
 
-        $this->enable = $this->request->getCookie(Configurator::COOKIE_SECRET) !== NULL;
-    }
+		$this->enable = $this->request->getCookie(Configurator::COOKIE_SECRET) !== NULL;
+	}
 
-    /**
-     * Nastavi mail panel
-     * @param string $mailPath
-     * @param boolean $mailPanel
-     */
-    public function setMail($mailPath, $mailPanel) {
-        $this->mailPath = $mailPath;
-        $this->mailPanel = $mailPanel;
-    }
+	/**
+	 * Nastavi mail panel
+	 * @param string $mailPath
+	 * @param boolean $mailPanel
+	 */
+	public function setMail($mailPath, $mailPanel)
+	{
+		$this->mailPath = $mailPath;
+		$this->mailPanel = $mailPanel;
+	}
 
-    /**
-     * Zapne nebo vypne mail panel
-     * @param boolean $enable
-     */
-    public function enableMail($enable = TRUE) {
-        $this->mailPanel = $enable;
-    }
+	/**
+	 * Zapne nebo vypne mail panel
+	 * @param boolean $enable
+	 */
+	public function enableMail($enable = TRUE)
+	{
+		$this->mailPanel = $enable;
+	}
 
-    /**
-     * Je debugger zapnuty (pomoci cookie)
-     * @return boolean
-     */
-    public function isEnabled() {
-        return $this->enable;
-    }
+	/**
+	 * Je debugger zapnuty (pomoci cookie)
+	 * @return boolean
+	 */
+	public function isEnabled()
+	{
+		return $this->enable;
+	}
 
-    /**
-     * Zapnuti debug modu pomoci cookie
-     */
-    public function enable() {
-        $this->response->setCookie(Configurator::COOKIE_SECRET, $this->cookie, strtotime('1 years'), '/', '', '', TRUE);
-        $this->enable = TRUE;
-    }
+	/**
+	 * Zapnuti debug modu pomoci cookie
+	 */
+	public function enable()
+	{
+		$this->response->setCookie(Configurator::COOKIE_SECRET, $this->cookie, strtotime('1 years'), '/', '', '', TRUE);
+		$this->enable = TRUE;
+	}
 
-    /**
-     * Vypnuti debug modu pomoci cookie
-     */
-    public function disable() {
-        $this->response->deleteCookie(Configurator::COOKIE_SECRET);
-        $this->enable = FALSE;
-    }
+	/**
+	 * Vypnuti debug modu pomoci cookie
+	 */
+	public function disable()
+	{
+		$this->response->deleteCookie(Configurator::COOKIE_SECRET);
+		$this->enable = FALSE;
+	}
 
-    /**
-     * Mail panel
-     */
-    private function mailer() {
-        if ($this->mailPanel) {
-            $mailer = new FileMailer($this->mailPath);
+	/**
+	 * Mail panel
+	 */
+	private function mailer()
+	{
+		if ($this->mailPanel) {
+			$mailer = new FileMailer($this->mailPath);
 
-            $service = 'mail.mailer';
-            $this->container->removeService($service);
-            $this->container->addService($service, $mailer);
+			$service = 'mail.mailer';
+			$this->container->removeService($service);
+			$this->container->addService($service, $mailer);
 
-            $this->container->getService('tracy.bar')
-                    ->addPanel(new MailPanel($this->mailPath, $this->request, $this->container->getService($service)));
-        }
-    }
+			$this->container->getService('tracy.bar')
+				->addPanel(new MailPanel($this->mailPath, $this->request, $this->container->getService($service)));
+		}
+	}
 
-    public function run() {
-        if (!Debugger::$productionMode) {
-            $this->mailer();
-        }
-    }
+	public function run()
+	{
+		if (!Debugger::$productionMode) {
+			$this->mailer();
+		}
+	}
 
 }
